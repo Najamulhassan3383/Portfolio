992f96a7eff7f1ccaa68dde00eeac5d2f765cfc6
Najamulhassan3383 <najmulhassan721@gmail.com>
2024-09-16 20:06:02 +0500

    added the techlist

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
 import Portfolio from "./components/portfolio/Portfolio";
-import TechList from "./components/techlist/Techlist";
+import TechList from "./components/techlist/TechList.jsx";
 const techListData = {
   title: "Tech Stack",
   items: [
